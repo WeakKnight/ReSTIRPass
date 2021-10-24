92c40fb7f976fddd0278991d249bb411897597d0
Tianyu Li <ltyucb@gmail.com>
2021-10-24 03:35:24 -0600

    Remove Dead Code

--- Helpers.h
+++ Helpers.h
@@ -7,7 +7,8 @@ void FillNeighborOffsetBuffer(uint8_t* buffer)
     uint32_t num = 0;
     float u = 0.5f;
     float v = 0.5f;
-    while (num < 8192 * 2) {
+    while (num < 8192 * 2) 
+    {
         u += phi2;
         v += phi2 * phi2;
         if (u >= 1.0f) u -= 1.0f;
@@ -15,8 +16,10 @@ void FillNeighborOffsetBuffer(uint8_t* buffer)
 
         float rSq = (u - 0.5f) * (u - 0.5f) + (v - 0.5f) * (v - 0.5f);
         if (rSq > 0.25f)
+        {
             continue;
-
+        }
+        
         buffer[num++] = int8_t((u - 0.5f) * R);
         buffer[num++] = int8_t((v - 0.5f) * R);
     }
